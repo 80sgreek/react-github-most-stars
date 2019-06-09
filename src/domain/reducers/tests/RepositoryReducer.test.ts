28536c7396154fceb4cadadc0d6d60c13b7d773a
Nick Thompson <80sgreek@gmail.com>
2019-06-09 23:02:55 +0100

    Fixed repo reducer test errors

--- src/domain/reducers/tests/RepositoryReducer.test.ts
+++ src/domain/reducers/tests/RepositoryReducer.test.ts
@@ -1,6 +1,5 @@
 import { repositoryReducer } from '../RepositoryReducer';
 import { RepositoryActionTypes, IRepositoryGetAllAction } from '../../actions/actionTypes';
-
 describe('RepositoryReducer', () => {
 
     it('returns the initial repositories state', () => {
@@ -20,8 +19,16 @@ describe('RepositoryReducer', () => {
     it('returns the correct updated repositories state from initial', () => {
 
         //ARRANGE
-        const action:IRepositoryGetAllAction = { type:RepositoryActionTypes.GET_ALL, repositories: [{ id: 123, name: 'test1name' }] };
-        const expectedState = { repositories: [{ id: 123, name: 'test1name' }] };
+        const testRepositories = [{ 
+            id: 123, 
+            full_name: 'test1name', 
+            created_at: new Date('December 17, 1995 03:24:00'), 
+            description: 'test1description', 
+            html_url: 'test1url', 
+            stargazers_count: 456 
+        }];
+        const action:IRepositoryGetAllAction = { type:RepositoryActionTypes.GET_ALL, repositories: testRepositories };
+        const expectedState = { repositories: testRepositories };
 
         //ACT
         const returnedState = repositoryReducer(undefined, action);
