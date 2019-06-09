87c90ee7608e1fbb6f060081294912a83144bc88
Nick Thompson <80sgreek@gmail.com>
2019-06-09 17:31:00 +0100

    removed trailing comma

--- src/actions/RepositoryActions.ts
+++ src/actions/RepositoryActions.ts
@@ -1,9 +1,9 @@
 import { ActionCreator, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import axios from 'axios';
-import { 
-  IRepository, 
-  IRepositoryState 
+import {
+  IRepository,
+  IRepositoryState
 } from '../reducers/RepositoryReducer';
 
 export enum RepositoryActionTypes {
@@ -18,13 +18,14 @@ export interface IRepositoryGetAllAction {
 export type RepositoryActions = IRepositoryGetAllAction;
 
 export const getAllRepositories: ActionCreator<
-  ThunkAction<Promise<any>, IRepositoryState, null, IRepositoryGetAllAction>> = () => {
+  ThunkAction<Promise<any>, IRepositoryState, null, IRepositoryGetAllAction>
+> = () => {
   return async (dispatch: Dispatch) => {
     try {
       const response = await axios.get('https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=3');
       dispatch({
         repositories: response.data.items,
-        type: RepositoryActionTypes.GET_ALL,
+        type: RepositoryActionTypes.GET_ALL
       });
     } catch (err) {
       console.error(err);
